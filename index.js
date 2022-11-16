f550bc90ae423c5a1d1d1dddc3e6635068855a72
Nathaniel Fischer <nathaniel.f@web.de>
2022-11-16 07:58:40 +0100

    v0.2

--- index.js
+++ index.js
@@ -18,26 +18,78 @@ $("#button-addon2").click(function() {
 
 
 // API GET request
-
 function callAPI(username) {
   // Open a new connection, using the GET request on the URL endpoint
   request.open("GET", "https://api.github.com/users/" + username, true);
 
+  // Send request
+  request.send()
+
   request.onload = function() {
     // Begin accessing JSON data here
     var data = JSON.parse(this.response)
-    updateWebsite(data);
-  }
 
-  // Send request
-  request.send()
+    //check for error
+    if (request.status >= 200 && request.status < 400) {
+      updateWebsite(data);
+    } else {
+      console.log("error");
+    }
+  }
 }
 
 
 
-// Data to HTML5
+// Data to HTML
 
 function updateWebsite(json) {
   console.log(json);
-  console.log("Avatar: " + json.avatar_url);
+  $(".user-image").attr("src", json.avatar_url);
+  $("#name").text(json.name);
+  $("#username").text("@" + json.login);
+  const date = new Date(json.created_at);
+  $(".joined-date").text("Joined " + date.toUTCString().substring(5, 17));
+  $(".description").text(json.bio); // what to do if empty?
+
+  //stats
+  $(".stats h2").eq(0).text(json.public_repos);
+  $(".stats h2").eq(1).text(json.followers);
+  $(".stats h2").eq(2).text(json.following);
+
+  //location
+  if (json.location === null) {
+    $(".data-point").eq(0).addClass("not-available");
+    $(".data-point a").eq(0).text("not available");
+  } else {
+    $(".data-point").eq(0).removeClass("not-available");
+    $(".data-point a").eq(0).text(json.location);
+  }
+
+  //twitter
+  if (json.twitter_username === null) {
+    $(".data-point").eq(1).addClass("not-available");
+    $(".data-point a").eq(1).text("not available");
+  } else {
+    $(".data-point").eq(1).removeClass("not-available");
+    $(".data-point a").eq(1).text(json.twitter_username);
+  }
+
+  //blog
+  if (json.blog === "") {
+    $(".data-point").eq(2).addClass("not-available");
+    $(".data-point a").eq(2).text("not available");
+  } else {
+    $(".data-point").eq(2).removeClass("not-available");
+    $(".data-point a").eq(2).text(json.blog);
+  }
+
+  //blog
+  if (json.company === null) {
+    $(".data-point").eq(3).addClass("not-available");
+    $(".data-point a").eq(3).text("not available");
+  } else {
+    $(".data-point").eq(3).removeClass("not-available");
+    $(".data-point a").eq(3).text(json.company);
+  }
+
 }
